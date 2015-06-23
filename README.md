63cd0cdb2bd5dd91cf2b8f305b8936700758aeff
Paul.Thornhill <development@paulthornhill.net>
2015-06-23 15:28:03 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Responsive Engagement Illinois
+It uses Sass, Compass, Susy 2.0 and shows a variety of techniques including dockable menus, responsive grids, tween and parallax effects, etc.
---
+++ builds/development/timeline-details/timeline_array.js
@@ -0,0 +1,2 @@
+var timeline1865 = ["charterday.html", "charterday-next.html", "charterday-testing.html", "charterday-last.html"];
+var timeline1875 = ["historypage1.html", "historypage3.html", "historypage3.html"];
